57b99d9485d7a4dc527a1be08a1f94501691cff1
Admin <admin@jeff-campos.local>
2021-06-12 18:30:31 -0300

    Feat(Logo): criando componente Logo, com testes e storybook

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,10 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Logo from '.'
+import { LogoProps } from 'components/Logo/index'
+
+export default {
+  title: 'Logo',
+  component: Logo
+} as Meta
+
+export const Default: Story<LogoProps> = (args) => <Logo {...args} />
