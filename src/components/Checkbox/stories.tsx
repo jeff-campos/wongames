e99de38ee1a98f22abd76872b37dcbe9f0d3a5f4
Admin <admin@jeff-campos.local>
2021-08-04 08:48:27 -0300

    Testes, Stories e outras melhorias no componente Checkbox

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -3,7 +3,44 @@ import Checkbox, { CheckboxProps } from '.'
 
 export default {
   title: 'Checkbox',
-  component: Checkbox
+  component: Checkbox,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  },
+  argTypes: {
+    onCheck: { action: 'checked' }
+  }
 } as Meta
 
-export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
+export const Default: Story<CheckboxProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        {...args}
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        {...args}
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        {...args}
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+      />
+    </div>
+  </>
+)
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,12 +1,15 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
 
 import theme from 'styles/theme'
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
   it('should render with label', () => {
-    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+    const { container } = renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" />
+    )
 
     // input a partir do papel / role
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
@@ -16,6 +19,8 @@ describe('<Checkbox />', () => {
 
     // label a partir do texto dela
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render without label', () => {
@@ -32,5 +37,41 @@ describe('<Checkbox />', () => {
     expect(screen.getByText(/checkbox label/i)).toHaveStyle({
       color: theme.colors.black
     })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} isChecked />)
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
+  })
+
+  it('should be accessible with tab', () => {
+    renderWithTheme(<Checkbox label="Checkbox" labelFor="Checkbox" />)
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+
+    expect(screen.getByLabelText(/checkbox/i)).toHaveFocus()
   })
 })
