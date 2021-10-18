16f851517a69aefb77ec6096f204d73174b0734f
Jeff Campos <jefferson.jarbas@gmail.com>
2021-10-18 09:02:35 -0300

    Criando a pagina wishlist

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,43 @@
+import Base from 'templates/Base'
+
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+import { Grid } from 'components/Grid'
+
+export type WishlistTemplateProps = {
+  games?: GameCardProps[]
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  games,
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Grid>
+    </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
+
+export default Wishlist
