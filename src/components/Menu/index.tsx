b1aec1dc41256f506d1ff23efd30ee652a923302
Jeff Campos <jefferson.jarbas@gmail.com>
2022-01-22 16:18:16 -0300

    pagina games e componente carticon

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -37,7 +37,7 @@ const Menu = ({ username }: MenuProps) => {
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
           <S.MenuLink href="/">Home</S.MenuLink>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          <S.MenuLink href="/games">Explore</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
 
