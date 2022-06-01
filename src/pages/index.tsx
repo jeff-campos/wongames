af87a2ba4f64f726e7198762df991777127f1fcd
Jeff Campos <jefferson.j.ext@e-ai.com.br>
2022-06-01 16:47:51 -0300

    inserindo apollo-client ao projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,17 @@ import highlightMock from 'components/Highlight/mock'
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
+// ATENÇÃO:
+// os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
+// getStaticProps => gerar estático em build time (gatsby)
+// getServerSideProps => gerar via ssr a cada request (nunca vai para o bundle do client)
+// getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do 1 req)
 
 export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo|leitura de context
+  // retorno dos dados
   return {
     props: {
       banners: bannersMock,
