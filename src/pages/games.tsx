cd0266303c32ca44a5f3f1bde62fb83b3d3ab74f
Jeff Campos <jefferson.j.ext@e-ai.com.br>
2022-06-27 20:45:17 -0300

    criando queries e chamadas graphQL para montar a home

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,9 +1,9 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
-import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
-
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -22,12 +22,10 @@ export async function getStaticProps() {
       revalidate: 60,
       games: data.games.map((game) => ({
         title: game.name,
+        slug: game.slug,
         developer: game.developers[0].name,
         img: `http://localhost:1337${game.cover!.url}`,
-        price: new Intl.NumberFormat('en', {
-          style: 'currency',
-          currency: 'USD'
-        }).format(game.price)
+        price: game.price
       })),
       filterItems: filterItemsMock
     }
